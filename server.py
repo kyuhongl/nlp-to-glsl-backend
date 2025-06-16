ae1b0a8b1ecc44eb2934a2f4d3b70d6d60dac9ca
Kyuhong Lee <kyuhongl@usc.edu>
2025-06-16 01:55:04 -0700

    rewrote CORS logic

--- server.py
+++ server.py
@@ -7,6 +7,15 @@ from dotenv import load_dotenv
 load_dotenv()
 app = FastAPI()
 
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 # Simple healthcheck
 @app.get("/")
